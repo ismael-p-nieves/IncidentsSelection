5e71c3483931fbe5f01818296f350414f8c41225
topcarto <71318954+topcarto@users.noreply.github.com>
2021-09-15 13:24:51 +0200

    Add files via upload

--- MVC/MinimumVertexCover.py
+++ MVC/MinimumVertexCover.py
@@ -1,3 +1,5 @@
+from math import ceil
+from math import log2
 from os import times
 import sys
 from datetime import datetime
@@ -18,10 +20,9 @@ def addControl(controles, idControl, idIncidencia):
             controles[i].append(int(idIncidencia))
             return controles
 
-    controles[idControl] = [int(idIncidencia)]   #Si el control no está en el diccionario, se crea una nueva entrada junto a la 
-                                            #incidencia.
+    controles[idControl] = [int(idIncidencia)]  #Si el control no está en el diccionario, se crea una nueva entrada junto a la 
+                                                #incidencia.
     return controles
-
 
 def createList(dataPath):
     incidencias = {}    #Las incidencias son las brechas de seguridad que surgen. El objetivo es seleccionar las incidencias
@@ -51,6 +52,8 @@ def createList(dataPath):
             incidencias[int(idInc[i])] = {'gravedad': int(grav[i]), 'tiempo': int(tiem[i])}
 
         controles = addControl(controles, idCon[i], int(idInc[i]))
+
+    print(controles)
     
     return incidencias, controles, dataframe
 
@@ -65,7 +68,21 @@ def createBQM(incidencias, controles):
     for i in idIncidencias:
         tiempos.append(incidencias[i]['tiempo'])
 
-    penalizacion = max(tiempos) + 1     #La penalización es el valor que nos va a ayudar a seleccionar qué incidencias tenemos
+    y = {}
+    slack_incidencias = {}
+
+    for k in idControles:
+        num_incidencias = len(controles[k])
+        num_slack = ceil(log2(num_incidencias))
+        slack_incidencias[k] = num_slack
+        for i in range(num_slack): 
+            if k in y:
+                y[str(k)].append(2**i)
+            else:
+                y[str(k)] = [2**i]
+                
+
+    penalizacion = max(tiempos) + 10    #La penalización es el valor que nos va a ayudar a seleccionar qué incidencias tenemos
                                         #que seleccionar. La condición que necesitamos que se cumpla es que todos los controles
                                         #sean resueltos. Cualquier solución que no cumpla con esa condición, será descartada
                                         #gracias al valor de penalización. Se ha decidido que el valor sea el máximo tiempo 
@@ -77,9 +94,8 @@ def createBQM(incidencias, controles):
     #esto es lo que determinará qué incidencias son las mejores en caso de que haya varias soluciones válidas. Se recomienda leer
     #la documentación para una explicación en mayor detalle y cómo se ha llegado a esta operación.
     for i, j in zip(idIncidencias, range(len(idIncidencias))):
-        Q.set_linear(str(i), tiempos[j])
+        Q.set_linear('x' + str(i), tiempos[j])
         J[(i, i)] = tiempos[j]
-        print(J[(i,i)])
 
     #Término linear xi (-P*xi).
     #Esta parte se utiliza para restar la penalización a cada incidencia que resuelva un control. De forma muy simplificada, si
@@ -90,23 +106,34 @@ def createBQM(incidencias, controles):
                                         #pueda usar para acceder al BQM.
         
         for i in cIncidencias:
-            key = (str(i))
+            key = ('x' + str(i))
 
             Q.linear[key] -= penalizacion
-            J[(i, i)] -= int(penalizacion)
-            print(J[i,i])
+            J[(i, i)] -= penalizacion
+
+    #Término linear y-y (sk² + 2Psk)
+    for k in idControles:
+        if k in y:
+            slacks = y[k]
+            if type(slacks) is int:
+                Q.set_linear('y' + str(k) + '-' + str(slacks), (slacks**2 + 2) * penalizacion)
+            else:
+                for i in slacks:
+                    Q.set_linear('y' + str(k) + '-' + str(i), (i**2 + 2) * penalizacion)
 
     #Término cuadrático xi-xj (2P*xi*xj).
     #Esta parte sirve simplemente para inicializar los términos cuadráticos en el BQM.
-    for i in range(len(idIncidencias)):
-        for j in range(i + 1, len(idIncidencias)):
-            incidencia1 = list(idIncidencias)[i]
-            incidencia2 = list(idIncidencias)[j]
+    for k in idControles:
+        cIncidencias = controles[k]
+        for i in range(len(cIncidencias)):
+            for j in range(i + 1, len(cIncidencias)):
+                incidencia1 = cIncidencias[i]
+                incidencia2 = cIncidencias[j]
             
-            key = (str(incidencia1), str(incidencia2))
+                key = ('x' + str(incidencia1),'x' + str(incidencia2))
                 
-            Q.quadratic[key] = 0
-            J[(incidencia1, incidencia2)] = 0
+                Q.quadratic[key] = 0
+                J[(incidencia1, incidencia2)] = 0
 
     #Término cuadrático xi-xj (2P*xi*xj).
     #El término cuadrático se encarga de evitar redundancias entre los controles. De forma muy simplificada, si un control es
@@ -119,11 +146,48 @@ def createBQM(incidencias, controles):
                 incidencia1 = cIncidencias[i]
                 incidencia2 = cIncidencias[j]
                 
-                key = (str(incidencia1), str(incidencia2))
+                key = ('x' + str(incidencia1),'x' + str(incidencia2))
                 
                 Q.quadratic[key] += 2 * penalizacion
                 J[(incidencia1, incidencia2)] += 2 * penalizacion
-                print(J[(incidencia1, incidencia2)])
+
+    #Término cuadrático yi-yj
+    for k in idControles:
+        if k in y:
+            slacks = y[k]
+            if type(slacks) is not int:
+                for i in range(len(slacks)):
+                    for j in range(i + 1, len(slacks)):
+                        slack1 = slacks[i]
+                        slack2 = slacks[j]
+
+                        key = ('y' + str(k) + '-' + str(i), 'y' + str(k) + '-' + str(j))
+                        Q.quadratic[key] = 2 * penalizacion * slack1 * slack2
+
+    #Término cuadrático y-x
+    for k in idControles:
+        if k in y:
+            cIncidencias = controles[k]
+            slacks = y[k]
+            if type(slacks) is int:
+                for i in range(len(cIncidencias)):
+                    incidencia1 = cIncidencias[i]
+
+                    key = ('x' + str(incidencia1), 'y' + str(k) + '-' + str(slacks))
+
+                    Q.quadratic[key] = -2 * penalizacion * slacks
+            else:
+                for i in range(len(cIncidencias)):
+                    for j in range(len(slacks)):
+                        incidencia1 = cIncidencias[i]
+                        slack1 = slacks[j]
+
+                        key = ('x' + str(incidencia1), 'y' + str(k) + '-' + str(j))
+
+                        Q.quadratic[key] = 2 * penalizacion * slack1
+
+    print(Q)
+        
     return Q, J
 
 def solve_knapsack(incidencias, controles, online, sampler=None, samplerSpin=None):
@@ -138,13 +202,14 @@ def solve_knapsack(incidencias, controles, online, sampler=None, samplerSpin=Non
         sampler = LeapHybridSampler()           #Se inicializa el sampler. El LeapHybridSampler se utiliza para conectarse a
     else:                                       #los ordenadores de D-Wave, y el ExactSolver para hacer una simulación en la
         sampler = ExactSolver()                 #computadora.
+        #Probar SimulatedAnnealingSampler
 
     timeSamplerF = datetime.now()
     timeSampler = timeSamplerF - timeSamplerS
     print('Se han tardado ' + str(timeSampler) + ' segundos en crear el sampler.')
 
     timeSolverS = datetime.now()
-    sampleset = sampler.sample(bqm)             #Se pasa el BQM que hemos creado anteriormnte al sampler y busca las soluciones.
+    sampleset = sampler.sample(bqm)             #Se pasa el BQM que hemos creado anteriormente al sampler y busca las soluciones.
     timeSolverF = datetime.now()
     timeSolver = timeSolverF - timeSolverS
     print('Se han tardado ' + str(timeSolver) + ' segundos en resolver el problema.')
@@ -152,29 +217,41 @@ def solve_knapsack(incidencias, controles, online, sampler=None, samplerSpin=Non
     sample = sampleset.first.sample             #Se guarda qué incidencias han sido gestionadas y cuáles no.
     energy = sampleset.first.energy             #Se guarda la energía que tiene la solución.
 
-    selected_item_ids = []
-    for varname, value in sample.items():
+    #selected_item_ids = []
+    #for varname, value in sample.items():
         # For each "x" variable, check whether its value is set, which
         # indicates that the corresponding item is included in the
         # knapsack
             # The index into the weight array is retrieved from the
             # variable name
-        if(sample[varname] == 1):
-            selected_item_ids.append(int(varname))
+        #if(sample[varname] == 1):
+            #selected_item_ids.append(int(varname))
+
+    selected_item_ids = []
+    for varname, value in sample.items():
+        # For each "x" variable, check whether its value is set, which
+        # indicates that the corresponding item is included in the
+        # knapsack
+        if value and varname.startswith('x'):
+            # The index into the weight array is retrieved from the
+            # variable name
+            selected_item_ids.append(int(varname[1:]))
 
     return sorted(selected_item_ids), energy, timeSolver, sampleset, J
 
 if __name__ == '__main__':
 
-    path = sys.argv[1] if len(sys.argv) > 1 else "/home/ismael/Escritorio/Cuantica/MVC/sample.csv"
+    path = sys.argv[1] if len(sys.argv) > 1 else "/home/ismael/Escritorio/Cuantica/MVC/Eventos50.csv"
 
-    if sys.argv[2].lower() != 'false' and str(sys.argv[2]).lower() != 'true':
-        print('Se ha introducido el modo del sampler incorrectamenete.\nRecuerde que para el sampler offline se utiliza "false" y para el online se utiliza "true".')
-        sys.exit()
-    if sys.argv[2].lower() == 'true':
-        online = True
-    else:
-        online = False
+    #if sys.argv[2].lower() != 'false' and str(sys.argv[2]).lower() != 'true':
+    #    print('Se ha introducido el modo del sampler incorrectamenete.\nRecuerde que para el sampler offline se utiliza "false" y para el online se utiliza "true".')
+    #    sys.exit()
+    #if sys.argv[2].lower() == 'true':
+    #    online = True
+    #else:
+    #    online = False
+
+    online = False
 
     incidencias, controles, dataframe = createList(path)
 
@@ -184,9 +261,9 @@ if __name__ == '__main__':
     timeTotal = timeTotalF - timeTotalS
     selected_time = list(dataframe.loc[selected_item_ids,'Tiempo'])
 
-    sampler = EmbeddingComposite(DWaveSampler(solver={'qpu': True}))
-    response = sampler.sample_qubo(J, num_reads=1000)
-    dwave.inspector.show(response)
+    #sampler = EmbeddingComposite(DWaveSampler(solver={'qpu': True}))
+    #response = sampler.sample_qubo(J, num_reads=1000)
+    #dwave.inspector.show(response)
 
     print("Encontrada solución con la energía {}".format(energy))
     print("Items seleccionados (0-indexed):", selected_item_ids)
